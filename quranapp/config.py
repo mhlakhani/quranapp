040e2c17a3af9abb94368bc77c7a160b65d7ca10
Hasnain Lakhani <m.hasnain.lakhani@gmail.com>
2011-09-19 20:50:39 +0500

    Removed one last SQLAlchemy reference.

--- quranapp/config.py
+++ quranapp/config.py
@@ -2,7 +2,6 @@ class DefaultConfig(object):
     DEBUG = False
     TESTING = False
     SECRET_KEY = '\xd0\xcc\xeeS\x0c\x0b\xffCH\xff\x9f\x9cG"m\xdde.\r\x02\x043-\xb4'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:////home/hasnain/programming/quranapp/db.sqlite3'
     DB_SURAH_KEY = 'quranapp:surahs'
     DB_AYAT_KEY = 'quranapp:ayats'
 
