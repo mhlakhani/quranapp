5d0b1d2fb9308b6f252d4c2412cf3b9f3a24f644
Hasnain Lakhani <m.hasnain.lakhani@gmail.com>
2011-09-19 18:03:18 +0500

    Updated models to use redis.

--- quranapp/models.py
+++ quranapp/models.py
@@ -1,43 +1,55 @@
 from quranapp import app
-from flask import Flask
-from flaskext.sqlalchemy import SQLAlchemy
+from flask import g
+try:
+    import cPickle as pickle
+except ImportError:
+    import pickle
 
-db = SQLAlchemy(app)
-
-class Surah(db.Model):
-    __table_args__ = {'mysql_engine':'InnoDB', 'mysql_charset':'utf8'}
-    id = db.Column(db.Integer, primary_key=True)
-    name_arabic = db.Column(db.Text)
-    name_english = db.Column(db.Text)
-    name_english_transliterated = db.Column(db.Text)
-    num_ayats = db.Column(db.Integer)
-    ayats = db.relationship('Ayat', backref='surah', lazy='dynamic')
+class Surah(object):
 
     def __init__(self, id, name_arabic, name_english, name_english_transliterated, num_ayats):
+
         self.id = id
         self.name_arabic = name_arabic
         self.name_english = name_english
         self.name_english_transliterated = name_english_transliterated
         self.num_ayats = num_ayats
 
+    def save(self):
+
+        st = pickle.dumps(self)
+        g.redis.hset('quranapp:surahs', self.id, st)
+
     def __repr__(self):
         return '<Surah %s: %s>' % (self.id, self.name_english)
 
-class Ayat(db.Model):
-    __table_args__ = {'mysql_engine':'InnoDB', 'mysql_charset':'utf8'}
-    id = db.Column(db.Integer, primary_key=True)
-    surah_id = db.Column(db.Integer, db.ForeignKey('surah.id'))
-    number = db.Column(db.Integer)
-    arabic = db.Column(db.Text)
-    english = db.Column(db.Text)
-    bismillah = db.Column(db.Text)
+    @staticmethod
+    def get(id):
 
-    def __init__(self, surah_id, number, arabic, english, bismillah):
+        st = g.redis.hget('quranapp:surahs', id)
+        return pickle.loads(st)
+
+class Ayat(object):
+
+    def __init__(self, id, surah_id, number, arabic, english, bismillah):
+
+        self.id = id
         self.surah_id = surah_id
         self.number = number
         self.arabic = arabic
         self.english = english
         self.bismillah = bismillah
 
+    def save(self):
+
+        st = pickle.dumps(self)
+        g.redis.hset('quranapp:ayats', self.id, st)
+
     def __repr__(self):
         return '<Surah %s Ayat %s>' % (self.surah_id, self.number)
+
+    @staticmethod
+    def get(id):
+
+        st = g.redis.hget('quranapp:ayats', id)
+        return pickle.loads(st)
