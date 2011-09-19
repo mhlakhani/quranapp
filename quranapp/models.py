65e692118a9584e440beb46fd5f1c3c9ca18b879
Hasnain Lakhani <m.hasnain.lakhani@gmail.com>
2011-09-19 18:50:56 +0500

    views.py now uses redis based models.

--- quranapp/models.py
+++ quranapp/models.py
@@ -1,38 +1,94 @@
 from quranapp import app
-from flask import g
+from flask import g, abort
+from math import ceil
 try:
     import cPickle as pickle
 except ImportError:
     import pickle
 
+class Pagination(object):
+
+    def __init__(self, items, page, per_page, total_count):
+        self.items = items
+        self.page = page
+        self.per_page = per_page
+        self.total_count = total_count
+
+    @property
+    def pages(self):
+        return int(ceil(self.total_count / float(self.per_page)))
+
+    @property
+    def has_prev(self):
+        return self.page > 1
+
+    @property
+    def has_next(self):
+        return self.page < self.pages
+
+    def iter_pages(self, left_edge=2, left_current=2,
+                   right_current=5, right_edge=2):
+        last = 0
+        for num in xrange(1, self.pages + 1):
+            if num <= left_edge or \
+               (num > self.page - left_current - 1 and \
+                num < self.page + right_current) or \
+               num > self.pages - right_edge:
+                if last + 1 != num:
+                    yield None
+                yield num
+                last = num
+
 class Surah(object):
 
-    def __init__(self, id, name_arabic, name_english, name_english_transliterated, num_ayats):
-
+    def __init__(self, id, name_arabic, name_english, name_english_transliterated, start_ayat, num_ayats):
         self.id = id
         self.name_arabic = name_arabic
         self.name_english = name_english
         self.name_english_transliterated = name_english_transliterated
+        self.start_ayat = start_ayat
         self.num_ayats = num_ayats
 
     def save(self):
-
         st = pickle.dumps(self)
         g.redis.hset('quranapp:surahs', self.id, st)
+
+    def get_ayats(self, page, per_page):
+        start = (page - 1) * per_page
+        if start > self.num_ayats:
+            abort(404)
+        end = min(start + per_page, self.num_ayats)
+        start += self.start_ayat
+        end += self.start_ayat
+        items = g.redis.hmget('quranapp:ayats', [x for x in xrange(start, end)])
+        items = map(pickle.loads, items)
+        return Pagination(items, page, per_page, self.num_ayats)
 
     def __repr__(self):
         return '<Surah %s: %s>' % (self.id, self.name_english)
 
     @staticmethod
     def get(id):
-
         st = g.redis.hget('quranapp:surahs', id)
         return pickle.loads(st)
+    
+    @staticmethod
+    def get_or_404(id):
+        st = g.redis.hget('quranapp:surahs', id)
+        if st is None:
+            abort(404)
+        return pickle.loads(st)
+
+    @staticmethod
+    def get_all():
+        ret = g.redis.hgetall('quranapp:surahs')
+        lis = ret.items()
+        lis.sort(key = lambda x: int(x[0]))
+        return map(pickle.loads, [x[1] for x in lis])
 
 class Ayat(object):
 
     def __init__(self, id, surah_id, number, arabic, english, bismillah):
-
         self.id = id
         self.surah_id = surah_id
         self.number = number
@@ -41,7 +97,6 @@ class Ayat(object):
         self.bismillah = bismillah
 
     def save(self):
-
         st = pickle.dumps(self)
         g.redis.hset('quranapp:ayats', self.id, st)
 
@@ -50,6 +105,9 @@ class Ayat(object):
 
     @staticmethod
     def get(id):
-
         st = g.redis.hget('quranapp:ayats', id)
         return pickle.loads(st)
+
+    @staticmethod
+    def get_many(ids):
+        return map(pickle.loads, [x for x in g.redis.hmget('quranapp:ayats', ids) if x is not None])
